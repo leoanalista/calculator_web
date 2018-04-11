325a42ca0e86b85dc293c5ce547c86ab41b39e1d
leoanalista <37988768+leoanalista@users.noreply.github.com>
2018-04-11 11:04:30 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+//call button
+document.getElementById("calculate-button").onclick = function() {
+
+    //Pegando os valores inseridos nos elementos
+    let elem1 = document.getElementById('elem-1');
+    let	elem2 = document.getElementById('elem-2');
+    
+    //Convertendo o tipo de varável
+    let val1 = parseInt(elem1.value);
+    let val2 = parseInt(elem2.value);
+
+    //Pegando o botão selecionado dentro do grupo "operation"
+    let choice = document.querySelector('input[name="operation"]:checked');
+    let operation = choice.value;
+
+	//Mostrando o resultado no elemento (h2) "Resultado"
+	let resultElement = document.getElementById('result');
+	resultElement.innerText = getResult(val1,val2,operation);
+
+	//Função para calculos
+	function getResult (n1, n2, operation) {
+
+		if (operation === '+') {
+			return n1 + n2;
+		} else if (operation === '-') {
+			return n1 - n2;
+		} else if (operation === '*') {
+			return n1 * n2;
+		} else if (operation === '/') {
+			return n1 / n2;
+		}
+	}	
+}
